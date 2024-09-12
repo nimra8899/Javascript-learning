e6cbde4dfdc71ea848e50e1935f8634f035ea044
Nimra Jabbar <105984076+nimra8899@users.noreply.github.com>
2024-09-12 12:58:10 -0700

    Add files via upload

---
+++ secondjs/second.js
@@ -0,0 +1,43 @@
+
+var b=777;
+console.log(b);
+
+//alert("hbjhbhb");
+let a=4;
+ a*=7;
+ console.log(a);
+
+ let d=34;
+let r =d>=20?"adult":"not adult"// ? is terninary operatio agr condition true ha to : se pehla wala print warna : k bad wala print
+console.log(r);
+let num;
+console.log(num);
+if(num%5==0)
+{
+    console.log("multiple of 5");
+
+}else{
+    console.log("not multiple");
+}
+//number k according grade mil jae
+let number =prompt("input number");
+if(number>80 && number<100)
+{
+    console.log("grade A");
+}
+if(number>70 && number<89)
+{
+    console.log("grade B");
+}
+if(number>60 && number<69)
+{
+    console.log("grade C");
+}
+if(number>50 && number<59)
+{
+    console.log("grade D");
+}
+if(number>0 && number<49)
+{
+    console.log("grade f");
+}
