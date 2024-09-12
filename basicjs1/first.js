86ac7237409bf06c1f0754f419535ef1eae1ea44
Nimra Jabbar <105984076+nimra8899@users.noreply.github.com>
2024-09-12 12:53:12 -0700

    Add files via upload

---
+++ basicjs1/first.js
@@ -0,0 +1,55 @@
+console.log("hellnnnnnnnnnnnnnnoo");
+var a;
+a=1;
+console.log(a);
+var b ='nimra';
+console.log("my nimngjhgjh"+a+b);
+var c="6";
+var d=6;
+if(c===d)
+{
+    console.log("true");
+
+}
+else{
+    console.log("false");
+}
+
+
+let o=34445;
+console.log(o);
+typeof b;
+const student={
+    name:"nujnn",
+    age:20,
+    study:19,
+    cgpa:7778
+};
+typeof student;
+console.log(student["study"]);
+student["age"]=student["age"]+2;
+console.log(student["age"]);
+console.log(student["name"]);
+student["name"]="nimra jj";
+console.log(student["name"]);
+const product={
+    name:"parker joter pen",
+    color:"black",
+    price:270,
+    discount:5,
+    rating:"4 star"
+};
+typeof product
+console.log(typeof product);
+console.log(typeof a);
+
+
+
+const profile={
+    name:"shhbhb",
+    post:194,
+    follower:564,
+    following:4,
+    bio:"apna college microsoft..."
+
+};
